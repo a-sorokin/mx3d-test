fb7e083d094e87f01938c6cf0bb6f9f71d9bc2c3
Alex Sorokin <alex@mx3d.com>
2025-07-11 17:14:56 +0200

    added util

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
 
 export const App = () => {
-  return <div>Hello</div>;
+  return <div>Hi!</div>;
 };
---
+++ src/utils/getArrayOfNumbers.ts
@@ -0,0 +1,10 @@
+export const getArrayOfNumbers = (
+  length: number,
+  maxNumber: number = 10,
+): number[] => {
+  const { floor, random } = Math;
+
+  return Array.from({ length }, () => {
+    return floor(random() * maxNumber);
+  });
+};
